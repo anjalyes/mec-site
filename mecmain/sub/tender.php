acb9c749b1ec3717e073aea8fee897c778184f6c
rohithkd <kdrohith@gmail.com>
2014-12-21 21:06:05 +0530

    Create tender.php

---
+++ mecmain/sub/tender.php
@@ -0,0 +1,37 @@
+<div id="student" class="img-responsive">
+
+
+	<h3>		Tenders </h3>
+		<div class="box1">
+			
+			<p>Some contents generally saying about tenders. Content team must add this </p>
+			
+			<br>
+			<br>
+
+			<a href="#">
+			<button type="button" class="btn btn"><i class="glyphicon glyphicon-comment"></i></button>
+				Sealed tenders in the prescribed format invited for the following under MODROBS project of Biomedical Department (Tender No :17/2014-15 dt 28.10.2014) </a>
+				<br>
+				<br>
+
+               <a href="#">
+			  <button type="button" class="btn btn"><i class="glyphicon glyphicon-comment"></i></button>
+				Sealed tenders in the prescribed format invited for the following under MODROBS project of Biomedical Department (Tender No :17/2014-15 dt 28.10.2014) </a>
+				<br>
+				<br>				
+			
+				
+			</ul>
+		
+	
+	
+			
+		</div>
+
+	
+
+</div>
+
+
+
