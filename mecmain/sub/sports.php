99103d322592ca6faf21c4204c12ad400901f18c
rohithkd <kdrohith@gmail.com>
2015-01-08 22:22:49 +0530

    Create sports.php

---
+++ mecmain/sub/sports.php
@@ -0,0 +1,44 @@
+<!--ABOUT US-->
+
+
+
+<div class="panel-group  panel-group-lists about-us" id="abvm" >
+
+              <div class="panel">
+                <div class="panel-heading">
+                  <h4 class="panel-title">
+                    <a  data-toggle="collapse" data-parent="#abvm" href="#aboutus">
+                      Sports
+
+                    </a>
+                     
+                   
+                  </h4>
+                </div>
+                <div id="aboutus" class="panel-collapse collapse in">
+                  <div class="panel-body">
+                    <p>
+                    Govt. Model Engineering College has an excellent physical education department under the guidance of Mr Johnson Phillip. The sports club of Model Engineering College is engaged in the training of the college team for various inter-collegiate events. The sports club conducts various sports and games events every year in the college. The intra-college leagues for games like football, basketball, volleyball, throw ball, chess, cricket and annual sports meet are the main events. The El-Juego, Inter-College Football League attracts participants from all over the state.
+				 	</p>
+				 	<p>Facilities of the department are: </p>
+	 <ul>
+	     <li>Health Club.</li>
+	     <li>FootBall Ground</li>
+		<li>Cricket nets</li>
+		<li>Table Tennis table</li>
+        <li>Basketball ground</li>
+		<li>Volleyball courts</li>
+		<li>Badminton court</li>
+		 </ul>
+
+		 <p>
+		 The department is engaged in the training of the college team for various inter-collegiate events.The college cricket team has always remained in top rung of University cricket matches.The college team has been the overall champions in the university sports a number of times. We also have the annual Sports Day in college, which is conducted near to the Arts Fest. It brings out the competitive spirit among the students.
+
+                  </div>
+                </div>
+              </div>
+
+             </div>
+
+
+</div><!-- panel group ends-->
